1d631a082b85b795018a4f22495b76b36fbcef02
geoff-raymann <odiwuorgeoff50@gmail.com>
2023-03-03 17:00:32 +0300

    Make changes

--- shop/models.py
+++ shop/models.py
@@ -13,7 +13,7 @@ class Category(TranslatableModel):
     class Meta:
         # ordering = ('name',)
         verbose_name = 'category'
-        verbose_name_plural = 'catgories'
+        verbose_name_plural = 'categories'
     
     def __str__(self):
         return self.name
@@ -30,7 +30,7 @@ class Product(TranslatableModel):
         description = models.TextField(blank=True)
     )
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/%Y/%m?%d', blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
